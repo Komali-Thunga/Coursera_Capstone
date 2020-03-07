1eb756e6b96bf00436b32f31ff2208eeca814c20
Komali-Thunga <61892530+Komali-Thunga@users.noreply.github.com>
2020-03-07 06:45:40 +0530

    Add files via upload

---
+++ Coursera_capstone_week1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM Data Science Professional Specialization - Coursera
+# 
+
+# This notebook will be mainly used for the capstone project : Specialization in Data Science of IBM       
+# Coursera platform : https://www.coursera.org/professional-certificates/ibm-data-science
+
+# In this notebook we import Pandas and Numpy library and print "Hello Capstone Project Course!".
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
